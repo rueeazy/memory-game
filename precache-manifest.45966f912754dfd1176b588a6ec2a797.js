da5226df95cffa213596e8d945fabe0f5b7cf61d
Christian Ruiz <santacruz.crm@gmail.com>
2020-09-30 12:19:05 -0700

    Updates

---
+++ precache-manifest.45966f912754dfd1176b588a6ec2a797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f49fb48ab3d8254eec0c55d47985473",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "ca6ee64d22d447cf1d09",
+    "url": "/memory-game/static/css/main.36f16dd8.chunk.css"
+  },
+  {
+    "revision": "38947cec4c200c9a574c",
+    "url": "/memory-game/static/js/2.de024441.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.de024441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6ee64d22d447cf1d09",
+    "url": "/memory-game/static/js/main.50d032b7.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
